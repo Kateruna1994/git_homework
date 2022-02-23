1dd54448d30e939250d7473153d783dc1e4bb75b
Kateruna1994 <ketringoncharuk1994@gmail.com>
2022-02-23 23:17:12 +0200

    Added value to the tab

---
+++ Piter.java
@@ -0,0 +1,9 @@
+public class Piter {
+    int age = 10;
+    int growth = 165;
+    String name = "Piter";
+
+    public void aboutPiter() {
+        System.out.println(name + growth + age);
+    }
+}
