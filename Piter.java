01bda3afb6160a6bcc8659683b4297a985c0a41c
Kateruna1994 <ketringoncharuk1994@gmail.com>
2022-02-23 23:34:11 +0200

    Added a new hair color variable

--- Piter.java
+++ Piter.java
@@ -2,6 +2,7 @@ public class Piter {
     int age = 25;
     int growth = 165;
     String name = "Piter";
+	String hair = "black"
 
     public void aboutPiter() {
         System.out.println(name + growth + age);
